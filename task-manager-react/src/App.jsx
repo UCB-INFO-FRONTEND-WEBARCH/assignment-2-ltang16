ea7c7c05753f7fc9a70474948a8520630b507167
Lauren Tang <ltang16@berkeley.edu>
2025-10-23 12:15:22 -0700

    minor changes

--- task-manager-react/src/App.jsx
+++ task-manager-react/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   // Remove task from the list
   const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id)); // Only show tasks that don't have the given ID
+    setTasks(tasks.filter(task => task.id !== id)); // Remove the task with the given ID
   }
 
   // Filter tasks based on completion status
