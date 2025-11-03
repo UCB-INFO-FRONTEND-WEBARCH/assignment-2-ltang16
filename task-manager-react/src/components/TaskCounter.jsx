e8f1bdef1f5bb33a23f8c2cccb56e9bb4786cae2
Lauren Tang <ltang16@berkeley.edu>
2025-11-03 12:52:23 -0800

    adding clarification in comments regarding task counter

--- task-manager-react/src/components/TaskCounter.jsx
+++ task-manager-react/src/components/TaskCounter.jsx
@@ -5,6 +5,9 @@ function TaskCounter({tasks, activeFilter}) {
     const totalTasks = tasks.length
     let currentTasks
 
+    {/* The way I have the task counter set up is that if I'm filtering on completed tasks, the counter will show 
+        completed tasks / total tasks, and if I'm filtering on active tasks, it'll show active / total. Otherwise
+        (displaying all tasks) it'll show total / total */}
     if (activeFilter === 'completed') {
         const current = tasks.filter(task => task.completed === true)
         currentTasks = current.length
